476ba3573463d4f7f56fdbb1e7cd5589c07f198f
Bohdan Stepanenko <stepanenko1905@gmail.com>
2022-08-25 10:50:07 +0300

    Create routes and fix redirect

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
+use App\Http\Controllers\JobVacancyController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', [JobVacancyController::class, 'getAllVacancies'])->name('all-vacancies');
+Route::get('/vacancy/{vacancy_id}', [JobVacancyController::class, 'showVacancy'])->name('vacancy');
+
+Route::get('/users', [UserController::class, 'getAllUsers'])->name('all-users');
+Route::get('/users/{user_id}', [UserController::class, 'getUser'])->name('user');
+
 Route::middleware('auth:sanctum')->group(function () {
-   Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/create-vacancy', [JobVacancyController::class, 'createVacancy'])->name('create-vacancy');
+    Route::post('/create-vacancy', [JobVacancyController::class, 'storeVacancy'])->name('store-vacancy');
+    Route::get('/edit-vacancy/{vacancy_id}', [JobVacancyController::class, 'editVacancy'])->name('edit-vacancy');
+    Route::put('/edit-vacancy', [JobVacancyController::class, 'updateVacancy'])->name('update-vacancy');
+    Route::get('/delete-vacancy/{vacancy_id}', [JobVacancyController::class, 'deleteVacancy'])->name('delete-vacancy');
+
+    Route::get('/response/{vacancy_id}', [ResponseController::class, 'sendResponse'])->name('send-response');
+    Route::get('/delete-response/{response_id}', [ResponseController::class, 'deleteResponse'])->name('delete-response');
 });
-
-
